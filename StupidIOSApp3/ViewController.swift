1cbfdb4c0d97bc12914826d0b26ce88465e6b5e1
JordanBurnett1210 <jabur1210@gmail.com>
2015-12-17 08:37:18 -0700

    Color change app on swift
    can choose between 3 default colors, or a random colored background.

--- StupidIOSApp3/ViewController.swift
+++ StupidIOSApp3/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func changeColor(sender: UIButton)
+    {
+        randomColorChange()
+    }
+    
+    func randomColorChange()
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000)
+        let newOppacity = CGFloat(Double(arc4random_uniform(101)) / 100.000)
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: newOppacity)
+    }
+    
+    @IBAction func greenColor(sender: UIButton)
+    {
+        view.backgroundColor = UIColor.greenColor()
+    }
+    
+    @IBAction func redColor(sender: UIButton)
+    {
+        view.backgroundColor = UIColor.redColor()
+    }
+    
+    @IBAction func blueButton(sender: UIButton)
+    {
+        view.backgroundColor = UIColor.blueColor()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
