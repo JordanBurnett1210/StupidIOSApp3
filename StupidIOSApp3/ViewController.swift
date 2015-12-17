4c9e18536bd47fe2954a916bc66ea0f7fd071b67
JordanBurnett1210 <jabur1210@gmail.com>
2015-12-17 07:47:48 -0700

    Initial Commit

---
+++ StupidIOSApp3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StupidIOSApp3
+//
+//  Created by Burnett, Jordan on 12/17/15.
+//  Copyright © 2015 Burnett, Jordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StupidIOSApp3Tests/StupidIOSApp3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  StupidIOSApp3Tests.swift
+//  StupidIOSApp3Tests
+//
+//  Created by Burnett, Jordan on 12/17/15.
+//  Copyright © 2015 Burnett, Jordan. All rights reserved.
+//
+
+import XCTest
+@testable import StupidIOSApp3
+
+class StupidIOSApp3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
